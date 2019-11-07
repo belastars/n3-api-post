b4ed5572a2e67af632f625b79ade3079347c169c
belastars <bela_baroni@hotmail.com>
2019-11-06 22:26:25 -0300

    falta fazer o desafio

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -13,7 +13,12 @@ const getAll = (request, response) => {
 
 const postAdd = (request,response) => {
 
-model.agenda.contatos.push(request.body)
+let contato = request.body
+contato.id = Math.random().toString(36).substr(-8)
+console.log(contato)
+model.agenda.contatos.push(contato)
+//console.log(request.body.nome)
+// model.agenda.contatos.push(request.body)
 //console.log(request.body)
 response.status(200).send()
 //model.agenda.contatos.push(contato)
